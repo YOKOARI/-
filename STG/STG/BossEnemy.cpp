853987f02b6e23b809752748a0713f1d8e39d75c
youtsuumochii <sugisugirr@gmail.com>
2017-05-29 11:04:55 +0900

    コメントふりました

--- STG/STG/BossEnemy.cpp
+++ STG/STG/BossEnemy.cpp
@@ -8,7 +8,10 @@
 #define SPOWN_X 350
 #define SPOWN_Y -20
 
+/****　BossEnemy(自身のID)　****/
 BossEnemy::BossEnemy(int id){
+
+	//生成時に設定されたIDを自身のIDとして格納
 	thisId = id;
 }
 
@@ -17,11 +20,14 @@ BossEnemy::~BossEnemy()
 {
 }
 
-
+/****　初期化処理　****/
 void BossEnemy::Initialize() {
+
+	//配列の初期化
 	for (i = 0; i < 20; i++) {
 		mBullet[i] = NULL;
 	}
+	//以下、各変数の初期化
 	bulletFlag = false;
 	bulletFlagCount = 0;
 	x = SPOWN_X;
@@ -39,18 +45,41 @@ void BossEnemy::Initialize() {
 	mCharaGraphicHandle[2] = LoadGraph(HP_BAR_AROUND);
 }
 
+/****　更新処理　****/
 void BossEnemy::Update() {
 	
+	//自身の座標更新
 	Move();
+
+	//自身の現在のｘ座標を、あたり判定を管理するクラスに登録
 	ControlGameInstance::GetInstance()->SetNewbeiEnemyPointX(this->x);
+
+	//自身の現在のｙ座標を、あたり判定を管理するクラスに登録
 	ControlGameInstance::GetInstance()->SetNewbeiEnemyPointY(this->y);
+
+	//弾の座標を更新
 	ShotUpdate();
+
+	//自身のあたり判定に使用する矩形をあたり判定を管理するクラスに登録
 	ControlGameInstance::GetInstance()->SetEnemyHitAria(x + 20, y + 28, 16);
+
+	//プレイヤーが発射したすべての弾において、そのいづれかが当たっているかどうかの処理
 	for (int j = 0; j < 70; j++) {
+
+		//プレイヤーの弾対自身のあたり判定を行う処理　TRUE：当たっている　FALSE：当たっていない
 		if (ControlGameInstance::GetInstance()->PlayerBulletToEnemyHitTest(j) == true) {
+
+			//デバッグ用　当たっていたら出力
 			DrawString(0, 0, "hitP", GetColor(255, 255, 255));
+
+			//自身のHPを減らす
 			hp--;
+
+			//もしHPが０になったら
 			if(hp == 0)
+
+				//自身が死んだことをあたり判定を管理するクラスの登録
+				//SetEnemyDeadFlag(自身のID､TRUE)
 				ControlGameInstance::GetInstance()->SetEnemyDeadFlag(thisId, true);
 			}
 		else {
@@ -60,21 +89,33 @@ void BossEnemy::Update() {
 
 }
 
+/****　描画処理　*****/
 void BossEnemy::Render() {
+
+	//HPバーの描画
 	if (hp >= 0) {
 		DrawExtendGraph(50, 50, 50 + hp, 50 + 40, mCharaGraphicHandle[1], TRUE);
 		DrawExtendGraph(50, 50, 50 + 680, 50 + 40, mCharaGraphicHandle[2], TRUE);
 	}
+
+	//自身の描画
 	DrawExtendGraph(x, y, x + 40, y + 56, mCharaGraphicHandle[0], TRUE);
+
+	//弾の描画
 	ShotRender();
 }
 
+/****　終了処理　****/
 void BossEnemy::Finalize() {
+
+	//画面外に座標を更新
 	x = 3000;
 	y = 3000;
 }
 
 void BossEnemy::Move() {
+
+	//フラグで移動するモーションを変更
 	if (moveFlag == 0) {
 		y += 2;
 		if (cnt > 100) moveFlag = 1;
@@ -89,8 +130,11 @@ void BossEnemy::Move() {
 		x += rand() % 3 - 1;
 		if (cnt % 50 == 0) moveFlag = 3;
 	}
+
+	//移動が終わったら弾を発射できるようにフラグをセット
 	else {
 		bulletFlag = true;
+		//100フレーム後に移動開始
 		if (cnt % 100 == 0) {
 			moveFlag = 1;
 			bulletFlag = false;
@@ -99,12 +143,17 @@ void BossEnemy::Move() {
 	cnt++;
 }
 
+/****　弾の更新処理　****/
 void BossEnemy::ShotUpdate() {
+
+	//配列にオブジェクトがある場合、更新処理をする
 	for (int i = 0; i < 20; i++) {
 		if (mBullet[i] != NULL) {
 			mBullet[i]->Update();
 		}
 	}
+
+	//インターバルが経過していてかつ移動が終わっていたら弾を発射
 	if (interval < cntBulletTime && bulletFlag == true) {
 		mBullet[bulletCnt] = new NormalEnemyBullet;
 		mBullet[bulletCnt]->Initialize();
@@ -116,7 +165,10 @@ void BossEnemy::ShotUpdate() {
 	if (bulletCnt == 20) bulletCnt = 0;
 }
 
+/****　弾の描画処理　****/
 void BossEnemy::ShotRender() {
+
+	//配列にオブジェクトがある場合、描画処理をする
 	for (int i = 0; i < 20; i++) {
 		if (mBullet[i] != NULL) {
 			mBullet[i]->Render();
--- STG/STG/BossEnemy.h
+++ STG/STG/BossEnemy.h
@@ -4,31 +4,31 @@
 #include "IBullet.h"
 class BossEnemy :public CharacterTask {
 public:
-	BossEnemy(int id);
+	BossEnemy(int id);	//コンストラクタで敵のIDを設定し管理
 	~BossEnemy();
 
-	virtual void Initialize() override;
-	virtual void Update() override;
-	virtual void Move() override;
-	virtual void Render() override;
-	virtual void Finalize() override;
+	virtual void Initialize() override;	//CharacterTask::Initialize()をオーバーライド（書き換え）
+	virtual void Update() override;		//CharacterTask::Update()をオーバーライド（書き換え）
+	virtual void Move() override;		//CharacterTask::Move()をオーバーライド（書き換え）
+	virtual void Render() override;		//CharacterTask::Render()をオーバーライド（書き換え）
+	virtual void Finalize() override;	//CharacterTask::Finalize()をオーバーライド（書き換え）
 
 private:
-	bool endShotFlag;
-	int moveX;
-	int moveY;
-	int i;
-	int hp;
-	bool bulletFlag;
-	int bulletFlagCount;
+	bool endShotFlag;		//弾を発射したかを管理するフラグ
+	int moveX;				//最終的な移動座標
+	int moveY;				//最終的な移動座標
+	int i;					//配列の管理
+	int hp;					//敵のHP
+	bool bulletFlag;		//弾を発射できるかを管理するフラグ
+	int bulletFlagCount;	//
 
-	int thisId;
-	int hitCount;
-	IBullet* mBullet[20];
-	void ShotUpdate();
-	void ShotRender();
-	int moveFlag;
-	int cnt;
-	int bulletCnt;
+	int thisId;				//自身のID
+	int hitCount;			//プレイヤーの弾が何度ヒットしたか
+	IBullet* mBullet[20];	//弾のオブジェクトを格納する配列
+	void ShotUpdate();		//弾の更新処理
+	void ShotRender();		//弾の描画処理
+	int moveFlag;			//移動するかを管理するフラグ
+	int cnt;				//移動した回数をカウントする変数
+	int bulletCnt;			//弾を打った後のインターバルをカウントする変数
 };
 
